67290dbd10b563dfb73b1d3112a886111ba04976
ZHBo <893629852@qq.com>
2020-04-08 18:32:50 +0800

    两数组中位数

--- src/main/java/com/rulinzi/arr/LCArray.java
+++ src/main/java/com/rulinzi/arr/LCArray.java
@@ -1,5 +1,9 @@
 package com.rulinzi.arr;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Department:  研发部
  * Title:       [com.rulinzi.arr — 模块名]
@@ -16,6 +20,9 @@ public class LCArray {
         int[] arrs = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
+                if(i==j){
+                    continue;
+                }
                 if(nums[i]+nums[j]==target){
                     arrs[0]=i;
                     arrs[1]=j;
@@ -25,4 +32,33 @@ public class LCArray {
         }
         return  null;
     }
+    /**
+     * @Description: 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+     *
+     * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+     *
+     * 你可以假设 nums1 和 nums2 不会同时为空。[1,3,4,5] [1,2,3,4]
+     * @Date:
+     **/
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length-i; j++) {
+                if (nums1[i]>=nums2[j]){
+                    list.add(nums2[j]);
+                }
+            }
+
+            list.add(nums1[i]);
+        }
+
+        return 3.5;
+    }
+
+    public static void main(String[] args) {
+        int[] x = new int[]{1,2,3,4};
+        int[] y = new int[]{2,3,4};
+        findMedianSortedArrays(x,y);
+    }
+
 }
